4581b8d6cd6406027f842e0841be61134bc5d54a
Ivan <769964834@qq.com>
2018-08-12 15:49:54 +0800

    update

--- database/migrations/2018_08_12_072918_create_commnet_table.php
+++ database/migrations/2018_08_12_072918_create_commnet_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLendTable extends Migration
+class CreateCommnetTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateLendTable extends Migration
      */
     public function up()
     {
-        Schema::create('lend', function (Blueprint $table) {
+        Schema::create('commnet', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateLendTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('lend');
+        Schema::dropIfExists('commnet');
     }
 }
